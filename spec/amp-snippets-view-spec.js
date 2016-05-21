3ee8c1778580f6c413051b4571090698372ebc85
Matthew Downey <mattddowney@gmail.com>
2016-05-21 16:04:44 -0600

    init

---
+++ spec/amp-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AmpSnippetsView from '../lib/amp-snippets-view';
+
+describe('AmpSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
