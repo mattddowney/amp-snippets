93f76b7968ab497c0374655dc1c7f3c409e2c02e
Matthew Downey <mattddowney@gmail.com>
2016-05-21 16:12:27 -0600

    remove unnecessary files/directories

--- spec/amp-snippets-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AmpSnippetsView from '../lib/amp-snippets-view';
-
-describe('AmpSnippetsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
